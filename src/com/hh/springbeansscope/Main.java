8d2c6b7a955227f388b317bd5b0b8dc94d649ba7
lhhernandez-isc <hhernandez.isc@gmail.com>
2023-01-12 10:55:51 -0600

    Se agregan metodos de inicializacion y destruccion en el bean TrackCoach

--- src/com/hh/springbeansscope/Main.java
+++ src/com/hh/springbeansscope/Main.java
@@ -6,20 +6,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		// load the spring config file
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"beanLifeCycle-applicationContext.xml");
 
 		// Retrieve bean from spring container
 		Coach coach = context.getBean("myCoach", Coach.class);
-		Coach coach2 = context.getBean("myCoach", Coach.class);
+		// Coach coach2 = context.getBean("myCoach", Coach.class);
 
 		// check if they are the same
-		boolean result = (coach == coach2);
+		// boolean result = (coach == coach2);
 
-//		prints out the result
-		System.out.println("\nPointing to the sabe object " + result);
+		// prints out the result
+		// System.out.println("\nPointing to the sabe object " + result);
+		//
+		// System.out.println("\nMemory location for coach: " + coach);
+		// System.out.println("\nMemory location for coach2: " + coach2);
 
-		System.out.println("\nMemory location for coach: " + coach);
-		System.out.println("\nMemory location for coach2: " + coach2);
+		System.out.println(coach.getDailyWorkout());
 
 		// close the context
 		context.close();
