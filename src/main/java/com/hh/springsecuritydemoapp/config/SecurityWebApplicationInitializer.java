8e4e0fa9e94dbe1bb58faadee3bb6dfb678c49c6
lhhernandez-isc <hhernandez.isc@gmail.com>
2023-01-18 17:35:54 -0600

    2: Configuracion de seguridad Basica, metodo @deprecated

---
+++ src/main/java/com/hh/springsecuritydemoapp/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.hh.springsecuritydemoapp.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
