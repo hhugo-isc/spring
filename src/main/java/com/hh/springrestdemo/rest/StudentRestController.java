436f51d042416b2ef7e35909ae99451f92f9b7bf
lhhernandez-isc <hhernandez.isc@gmail.com>
2023-01-19 17:45:21 -0600

    2: Se agrega entidad Student y controlador StudentRestController para devolver java POJOs como JSON

---
+++ src/main/java/com/hh/springrestdemo/rest/StudentRestController.java
@@ -0,0 +1,27 @@
+package com.hh.springrestdemo.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hh.springrestdemo.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+//	define endpoint for "/students" - return list of students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		List<Student> students = new ArrayList<>();
+
+		students.add(new Student("John", "Doe"));
+		students.add(new Student("Mary", "Public"));
+		students.add(new Student("Mario", "Rossi"));
+		return students;
+	}
+
+}
