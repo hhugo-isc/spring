769957455be6d2e095c0ef30ad1a0417c40d9e27
lhhernandez-isc <hhernandez.isc@gmail.com>
2023-01-20 16:22:57 -0600

    2: Se agrega endpoint '/customers/{customerId}'

--- src/main/java/com/hh/springrest/controller/CustomerRestController.java
+++ src/main/java/com/hh/springrest/controller/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +20,15 @@ public class CustomerRestController {
 	private CustomerService customerService;
 
 //	add mapping for GET /customers
-
 	@GetMapping("/customers")
 	public List<Customer> getCustomers() {
 		return customerService.getCustomers();
+	}
+
+//	add mapping for GET /customers/{customerId}
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable("customerId") int customerId) {
+		return customerService.getCustomer(customerId);
 	}
 
 }
