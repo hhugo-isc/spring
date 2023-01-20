bc4ef514ad1c29bb7318cd75711c226795a3f726
lhhernandez-isc <hhernandez.isc@gmail.com>
2023-01-19 18:03:01 -0600

    3: Se refactoriza codigo, se agrega dependeicia para anotaciones de javax y se agrega endpoint para devolver un objeto Student a partir de su id

--- src/main/java/com/hh/springrestdemo/rest/StudentRestController.java
+++ src/main/java/com/hh/springrestdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.hh.springrestdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +16,27 @@ import com.hh.springrestdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
-//	define endpoint for "/students" - return list of students
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> students = new ArrayList<>();
+	private List<Student> students;
 
+//	define @postConstruct to load the student data ... only once
+	@PostConstruct
+	public void loadData() {
+		this.students = new ArrayList<>();
 		students.add(new Student("John", "Doe"));
 		students.add(new Student("Mary", "Public"));
 		students.add(new Student("Mario", "Rossi"));
-		return students;
+	}
+
+//	define endpoint for "/students" - return list of students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		return this.students;
+	}
+
+//	define endpoint for "/students/{studentId}" - return Student with id = studentId
+	@GetMapping("/students/{studentId}")
+	public Student getStudents(@PathVariable int studentId) {
+		return students.get(studentId);
 	}
 
 }
