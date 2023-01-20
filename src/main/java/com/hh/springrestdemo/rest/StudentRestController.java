4807be40211f77246c52f312ffe9670fbbc68f26
lhhernandez-isc <hhernandez.isc@gmail.com>
2023-01-20 13:52:29 -0600

    5: Refactorizacion del codigo para usar la anotacion @ControllerAdvice, manejo de excepciones personalizado => parte 2

--- src/main/java/com/hh/springrestdemo/rest/StudentRestController.java
+++ src/main/java/com/hh/springrestdemo/rest/StudentRestController.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,18 +40,6 @@ public class StudentRestController {
 			throw new StudentNotFoundException("Student id not found - " + studentId);
 		}
 		return students.get(studentId);
-	}
-
-//	add an exception handler using @ExceptionHandler
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
-//		create StudentErrorResponse
-		StudentErrorResponse errorResponse = new StudentErrorResponse();
-		errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-		errorResponse.setMessage(ex.getMessage());
-		errorResponse.setTimeStamp(System.currentTimeMillis());
-
-		return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
 	}
 
 }
