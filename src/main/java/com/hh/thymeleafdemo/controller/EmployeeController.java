ad1750f8a55236ef9deed967a04ffd80727b060e
lhhernandez-isc <hhernandez.isc@gmail.com>
2023-01-23 16:45:15 -0600

    5: Se agrega ruta para mostrar formulario para mostrar formulario para agregar un empleado, se agrega la pagina html del formulario y codigo java para manejar el posteo. Se agrega metodo a EmployeeRepository para ordenar el resultado por lastName ascendentemente

--- src/main/java/com/hh/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/hh/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hh.thymeleafdemo.entity.Employee;
@@ -26,7 +28,21 @@ public class EmployeeController {
 //		add to the spring model
 		List<Employee> employees = employeeService.findAll();
 		model.addAttribute("employees", employees);
-		return "list-employees";
+		return "employees/list-employees";
+	}
+
+	@GetMapping("showFormForAdd")
+	public String showFormForAdd(Model model) {
+//		create and add attribute to the model
+		model.addAttribute("employee", new Employee());
+
+		return "employees/employee-form";
+	}
+
+	@PostMapping("save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		employeeService.save(employee);
+		return "redirect:/employees/list";
 	}
 
 }
--- src/main/java/com/hh/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/hh/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,10 +1,14 @@
 package com.hh.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hh.thymeleafdemo.entity.Employee;
 
 //@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	public List<Employee> findAllByOrderByLastNameAsc();
 
 }
