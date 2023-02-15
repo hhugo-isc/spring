8ab28ec99c7d9b2a0229477610a5d18970db0745
lhhernandez-isc <hhernandez.isc@gmail.com>
2023-02-15 15:50:45 -0600

    Configuracion inicial del proyecto

---
+++ src/main/java/com/hh/easybanckbackend/EasybanckbackendApplication.java
@@ -0,0 +1,13 @@
+package com.hh.easybanckbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybanckbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasybanckbackendApplication.class, args);
+	}
+
+}
